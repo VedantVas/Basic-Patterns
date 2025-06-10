7f78d0b3b52134d1d2c71d4bd0bf660e3affffb2
Vedant Vashishtha <vedantvashish@gmail.com>
2025-06-10 22:45:00 +0530

    pattern18.cpp

--- pattern18.cpp
+++ pattern18.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void pattern17(int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){//base condition
         for(char ch ='E'-i;ch <='E';ch++){
-            cout<<ch;
+            cout<<ch;//printing character 
         }
         cout<<endl;
     }
@@ -13,6 +13,6 @@ void pattern17(int n){
 int main(){
     int n;
     cin>>n;
-    pattern17(n);
+    pattern17(n);//function calling 
     return 0;
 }
